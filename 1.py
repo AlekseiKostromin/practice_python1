aaebfeb946be45537f9a39af1b1b20494c4c53ca
Aleksei Kostromin <cs777@yandex.ru>
2024-09-17 10:55:43 +0500

    222
    222

--- 1.py
+++ 1.py
@@ -1,1 +1,12 @@
-print("hello")
+#name = input("Введите ваше имя:")
+#print("Ваше имя:", name)
+
+#a = 5
+#b = 20
+#c = a + b
+#print(a+b)
+
+name = input("Ваше имя:")
+second_name = input("Ваша фамилия:")
+
+print("Вы:", name, "Ваша фамилия:", second_name)
